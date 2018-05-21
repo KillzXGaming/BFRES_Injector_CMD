c098bb4b352c1c3a1c3d6db8f67d07d2114a240d
KillzXGaming <millernathan35@gmail.com>
2018-05-21 10:26:59 -0400

    Add UI for selectiing Models/Meshes

---
+++ BFRES_Injector_CMD/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syroot.NintenTools.Bfres;
+
+namespace BFRES_Injector_CMD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public ResFile TargetBFRES = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "bfres Files (BFRES)|*.BFRES";
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    TargetBFRES = new ResFile(ofd.FileName);
+
+                    comboBox1.Items.Clear();
+                    comboBox2.Items.Clear();
+
+                    foreach (Model mdl in TargetBFRES.Models.Values)
+                    {
+                        comboBox1.Items.Add(mdl.Name);
+                    }
+                    comboBox1.SelectedIndex = 0;
+
+                }
+                catch
+                {
+                    MessageBox.Show("Could not read BFRES file");
+                }
+            }
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog obj = new OpenFileDialog();
+            obj.Filter = "obj Files (OBJ)|*.OBJ";
+            if (obj.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    BFRES.MeshObj test = new BFRES.MeshObj();
+
+                    test.ReadObj(obj.FileName);
+                    TargetBFRES.Models[0].Materials[0].RenderState.PolygonControl.CullBack = false;
+                    TargetBFRES.Models[0].Materials[0].RenderState.PolygonControl.CullFront = false;
+                    TargetBFRES.Models[0].Materials[0].RenderState.PolygonControl.PolygonModeEnabled = true;
+                    test.InjectMesh(TargetBFRES.Models[0], TargetBFRES.ByteOrder);
+                    TargetBFRES.Name = "A_Cool_Mesh";
+
+                    SaveFileDialog SvBFRES = new SaveFileDialog();
+                    SvBFRES.Filter = "bfres Files (BFRES)|*.BFRES";
+                    if (SvBFRES.ShowDialog() == DialogResult.OK)
+                    {
+                        TargetBFRES.Save(SvBFRES.FileName);
+                        MessageBox.Show("Saved bfres file successfully!");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Could not read OBJ file");
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BFRES.FMDLIndex = comboBox1.SelectedIndex;
+
+            comboBox2.Items.Clear();
+            comboBox2.ResetText();
+            foreach (Shape shp in TargetBFRES.Models[comboBox1.SelectedIndex].Shapes.Values)
+            {
+                comboBox2.Items.Add(shp.Name);
+            }
+            comboBox2.SelectedIndex = 0;
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BFRES.ShapeIndex = comboBox2.SelectedIndex;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- BFRES_Injector_CMD/Properties/AssemblyInfo.cs
+++ BFRES_Injector_CMD/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BFRES_Injector_CMD")]
@@ -10,26 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BFRES_Injector_CMD")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cf033e6a-996a-4d4f-909c-e7426a356266")]
+[assembly: Guid("adc3404b-e1d4-43e5-9bdb-2800b55db84b")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
